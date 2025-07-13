41a990cac001799b6ea5577c33595f78c97b77a1
renaissancejlc <91338859+renaissancejlc@users.noreply.github.com>
2025-07-13 14:40:49 -0700

    Update DarftOrder.jsx

--- src/pages/DarftOrder.jsx
+++ src/pages/DarftOrder.jsx
@@ -10,7 +10,7 @@ const players = [
   { name: 'Callie', guess: { player: 'Cal Raleigh', homeruns: 52 } },
   { name: 'Kevin', guess: { player: 'Junior Caminero', homeruns: 50 } },
   { name: 'Simon', guess: { player: 'Matt Olson', homeruns: 50 } },
-  { name: 'River', guess: null },
+  { name: 'River', guess: { player: 'Cal Raleigh', homeruns: 55 } },
   { name: 'Christian', guess: { player: 'Oneil Cruz', homeruns: 57 } },
   { name: 'Utsav Roommate', guess: { player: 'Cal Raleigh', homeruns: 39 } },
   { name: 'Tariq', guess: { player: 'Oneil Cruz', homeruns: 55 } },
